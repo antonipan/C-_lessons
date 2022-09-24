3d3b7a6be282745e2231e7542dfa95d4cbfdf5e8
Anton P <a.s.panferov@mail.ru>
2022-09-24 13:43:51 +0300

    Всё добавлено

---
+++ Urok_1/Program.cs
@@ -0,0 +1,117 @@
+﻿// Пример №1. 
+
+Console.WriteLine("Hello, World! 234");
+
+// Пример №2. 
+Console.WriteLine("Введите ваше имя");
+string user_name = Console.ReadLine();
+Console.Write("Здравствуйте, ");
+Console.Write(user_name);/ Пример №3. 
+
+// Пример №3.
+
+
+int numberA = 14;
+int numberB = 5;
+int sum = numberA + numberB;
+
+Console.WriteLine(sum);
+
+// Пример №4. 
+int numberA = new Random().Next(1,10); // числа от одного до десяти
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int diff = numberA + numberB;
+Console.WriteLine(diff);
+
+// Пример №5. 
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
+
+// Пример №6. 
+int a = 2;
+int b = 4;
+int c = 7;
+int d = 1;
+int e = 5; 
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("это максимальное = ");
+Console.Write(max);
+
+// Пример №7. 
+Console.Clear();
+//Console.SetCursorPosition(10,4); 
+//Console.WriteLine("+");
+
+int xa = 40, ya = 1, 
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = yb;
+
+int count = 0;
+while(count < 20000)
+{
+    int what = new Random().Next(0, 3);
+    if(what == 0)
+    {
+        x = (x+xa)/2;
+        y = (y+ya)/2;
+    }
+    if (what == 1)
+    {
+        x = (x+xb)/2;
+        y = (y+yb)/2;
+    }
+    if (what == 2)
+    {
+        x = (x+xc)/2;
+        y = (y+yc)/2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
+
+// Пример №8. 
+int Max (int arg1, int arg2, int arg3) 
+{
+    int result = arg1;
+    if (arg2>result) result = arg2;
+    if (arg3>result) result = arg3;
+    return result;
+}
+int [] array = {1, 3, 18, 48, 35, 61, 102, 79, 50, 35, 64}; 
+
+
+Console.WriteLine();
+
+
+
+
+// Пример №9. 
---
+++ Urok_2/Program1.cs
@@ -0,0 +1,96 @@
+﻿//Пример №9
+
+int Max (int arg1, int arg2, int arg3) 
+{
+    int result = arg1;
+    if (arg2>result) result = arg2;
+    if (arg3>result) result = arg3;
+    return result;
+}
+int [] array = {1, 3, 18, 48, 35, 61, 102, 79, 50}; 
+
+array[0] = 12; 
+
+int result = Max (
+    Max(array[0], array[1], array [2]),
+    Max(array[3], array[4], array [5]),
+    Max(array[6], array[7], array [8])    
+);
+
+Console.WriteLine(result);
+
+
+// Пример №10
+int [] array = {1,5,5,5,6,7,8,5,14,5,23};
+
+int n = array.Length;
+
+int find = 5;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+}
+
+// Пример №11.
+Console.Clear();
+void FillArray(int[] collection) 
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index]=new Random().Next(1, 10);
+        index++;
+    
+    }
+}
+
+void PrintArray(int[] coll)
+{
+    int count = coll.Length;
+    int position = 0; 
+    while (position < count)
+    {
+        Console.WriteLine(coll[position]);
+        position++;
+    }
+}
+// retutn не используем
+
+int Indexof(int[]collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0; 
+    
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            Console.WriteLine(position);
+        }
+        index++;
+    }
+    return position; 
+}
+int [] array = new int [10];
+FillArray(array);
+array[4] = 5;
+array[7] = 5;
+PrintArray(array);
+
+Console.WriteLine("А теперь позиция");
+
+int pos = Indexof(array, 5);
+Console.WriteLine(pos);
+
+
